95d41c99080306c70b11dea03b0da12dee027884
aliensmart <kaoua17@gmail.com>
2020-05-07 02:12:48 -0400

    edited urls.py and created a new about.html

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import (TemplateView, View,
+                                DeleteView, DetailView,
+                                UpdateView, CreateView)
 
 # Create your views here.
+class AboutView(TemplateView):
+    template_name = 'about.html'
