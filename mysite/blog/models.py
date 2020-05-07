0be9a4d313ab93ab46264fa42765dfe895e24f42
aliensmart <kaoua17@gmail.com>
2020-05-07 02:07:10 -0400

    setting.py edited with template dir adition and static dir

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
         return self.title
     
 
-class comment(models.Model):
+class Comment(models.Model):
     port = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
